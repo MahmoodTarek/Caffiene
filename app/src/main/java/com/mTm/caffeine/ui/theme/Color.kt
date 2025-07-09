7dce0539030711b12de1dbbf5960b81c6a964199
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-07-09 12:31:05 +0300

    add colors and fonts

---
+++ app/src/main/java/com/mTm/caffeine/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.mTm.caffeine.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val White = Color(0xFFF5F5F5)
+val Off_white = Color(0xFFFFEEE7)
+val Brown = Color(0xFF7C351B)
+val DarkBrown = Color(0xFF291710)
+val LightBrown = Color(0xFFB94B23)
+val Black = Color(0xFF030004)
+val DarkGray = Color(0xFF1F1F1F)
+val Gray = Color(0xFF3B3B3B)
+val LightGray = Color(0xFFB3B3B3)
+
